be56a01f593e63baaf6bfeaeedb0148ca2dcd523
Cesar <cesarhcliment@gmail.com>
2020-11-13 13:08:29 +0100

    Change digits.cs

--- p2_source_files/digits.cs
+++ p2_source_files/digits.cs
@@ -1,35 +1,47 @@
+/*
+ * Counters of number of digits
+ * 	digits1 is for 1 digit, 
+ * 	digits2 is for 2 digits, 
+ * 	digits3 is for 3 digits 
+ * 	digits4 is for more than 3 digits
+ * Cesar Higuero 1º DAW SEMI
+ * Development Environments
+ */
 using System;
 
-class digits
+public class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
-        }
-      e = Console.ReadLine();
+	public static void Main()
+	{
+		string numText;
+		int number;
+		int digits1 = 0, digits2 = 0, digits3 = 0, digits4 = 0;
+
+		do
+		{
+			Console.Write("Insert a number (end to finish): ");
+			// read the number as string to check the word "end"
+			numText = Console.ReadLine();
+
+			if (numText != "end")
+			{
+				number = Convert.ToInt32(numText);
+				
+				if (number / 10 == 0)
+					digits1++;
+				else 
+				if (number / 100 == 0)
+					digits2++;
+				else
+				if (number / 1000 == 0)
+					digits3++;
+				else
+					digits4++;
+			}
+		}
+		while (numText != "end");
+
+		Console.WriteLine("Size in digits -> one: {0} two: {1} three: {2} more: {3}", 
+							digits1, digits2, digits3, digits4); 
+	}
 }
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
-}
-
