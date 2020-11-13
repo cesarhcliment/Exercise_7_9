5453c39655bafb796ff0b09de0aa0344ecbf093b
Cesar <cesarhcliment@gmail.com>
2020-11-13 13:05:20 +0100

    Change average.cs source file.

--- p2_source_files/average.cs
+++ p2_source_files/average.cs
@@ -1,20 +1,39 @@
+/*
+ * Calculate the average of some numbers
+ * Cesar Higuero 1º DAW SEMI
+ * Development Environments
+*/
+
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+
+public class Average
+{
+	public static void Main()
+	{
+		string markText;
+		double mark;
+		int counter = 0;
+		double sum = 0;
+		double average;
+
+		do 
+		{
+			Console.Write("Insert a mark (end to finish): ");
+			markText = Console.ReadLine();	// we read the mark as string to check the word "end"	 
+			
+			if (markText != "end")
+			{
+				mark = Convert.ToDouble(markText);
+				counter++;   
+				sum += mark; 
+			}
+			
+		} while(markText != "end");
+		
+		if (counter > 0)
+		{
+			average = sum / counter;  
+			Console.WriteLine("The average is {0}.", average);
+		}
+	}
 }
